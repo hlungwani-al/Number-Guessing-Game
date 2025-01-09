80ebc976561e9543d6610883f69c5053b26c830e
hlungwani-al <hlungwani.al6@gmail.com>
2025-01-09 10:01:17 +0200

    added restart, still lacking checking of string

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -6,21 +6,46 @@ class RandomNumberGenerator {
     void numberGusser() {
         Scanner userInput = new Scanner(System.in);
         byte attempts = 5;
+        Byte userAttempts[] = { -1, -1, -1, -1, -1 };
         boolean gameStatus = true;
-        while (gameStatus && attempts >= 0) {
+        while (gameStatus && attempts > 0) {
             System.out.println("You have: " + attempts + " attempt(s). \nGuess a number from 0 to 100: ");
             byte userGuess = userInput.nextByte();
-
+            userAttempts[attempts - 1] = userGuess;
+            attempts--;
             if (userGuess == Number) {
                 System.out.println("Congrats you guessed right.\n");
                 gameStatus = false;
             } else {
-                if (userGuess > Number) {
-                    System.out.println("\nooops, wrong guess. try going lower\n");
+                if (attempts == 0) {
+                    Scanner stringScanner = new Scanner(System.in);
+                    System.out.print("You are out of attempts!, your attempts are: ");
+                    for (int index = 4; index >= 0; index--) {
+                        System.out.print(userAttempts[index] + " ");
+                    }
+                    System.out.println("\nThe correct number is: " + Number
+                            + "\nDo you wish to restart ? (y)es or n(o)");
+                    String restartGame = stringScanner.nextLine();
+                    restartGame.toLowerCase();
+                    stringScanner.close();
+
+                    // System.out.println(restartGame + " is not equal to " +
+                    // restartGame.toLowerCase());
+
+                    if (restartGame == "y" || restartGame == "yes") {
+                        System.out.println("Game restarting ...\n...\n");
+                        numberGusser();
+                    } else {
+                        System.out.println("Thanks for playing, better luck next time. Good bye");
+                    }
                 } else {
-                    System.out.println("\nooops, wrong guess. try going higher\n");
+                    if (userGuess > Number) {
+                        System.out.println("\nooops, wrong guess. try going lower\n");
+                    } else {
+                        System.out.println("\nooops, wrong guess. try going higher\n");
+                    }
                 }
-                attempts--;
+
             }
 
         }
